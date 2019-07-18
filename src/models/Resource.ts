9c2abc3d10af4960ba0d3061f30eabe753c2da60
AlfredYang1986 <yangyuanpig@gmail.com>
2019-07-18 16:48:29 +0800

    resource model update

--- src/models/Resource.ts
+++ src/models/Resource.ts
@@ -9,6 +9,30 @@ class Resource extends Typegoose implements IModelBase<Resource> {
 
     @prop({ required: true })
     public gender: number
+    
+    @prop({ required: true })
+    public age: number
+    
+    @prop({ required: true })
+    public education: string
+    
+    @prop({ required: true })
+    public professional: string
+    
+    @prop({ required: true })
+    public advantage: string
+    
+    @prop({ required: true })
+    public evaluation: string
+    
+    @prop({ required: true })
+    public experience: number
+    
+    @prop({ required: true })
+    public totalTime: number
+    
+    @prop({ required: true })
+    public entryTime: number
 
     @prop({ ref: Image, required: true })
     public avatar: Ref<Image>
