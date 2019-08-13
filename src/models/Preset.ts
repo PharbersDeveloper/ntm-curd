530d709c91fc776078b97844698322281b3c8a53
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-13 11:22:59 +0800

    修改一下presets

--- src/models/Preset.ts
+++ src/models/Preset.ts
@@ -3,23 +3,45 @@ import { prop, Ref, Typegoose } from "typegoose"
 import Hospital from "./Hospital"
 import IModelBase from "./modelBase"
 import Product from "./Product"
+import Resource from "./Resource"
 
 class Preset extends Typegoose implements IModelBase<Preset> {
 
     @prop({ref: Product, required: true})
-    public product: Ref<Product>
+    public product?: Ref<Product>
 
     @prop({ref: Hospital, required: true})
-    public hospital: Ref<Hospital>
+    public hospital?: Ref<Hospital>
+    
+    @prop({ref: Resource, required: false})
+    public resource?: Ref<Resource>
 
     @prop({ default: 0})
-    public salesQuota: number
+    public salesQuota?: number   // p_quota
 
     @prop({ default: 0})
-    public potential: number
-
+    public achievements?: number     // p_sales
+    
     @prop({ default: 0})
-    public achievements: number
+    public potential?: number    // 铁马不变
+    
+    @prop({ default: 0})
+    public share?: number    // p_share
+
+    @prop({ default: ""})
+    public territoryManagementAbility?: string // p_territory_management_ability
+    
+    @prop({ default: ""})
+    public salesSkills?: string // p_sales_skills
+    
+    @prop({ default: ""})
+    public productKnowledge?: string // p_product_knowledge
+    
+    @prop({ default: ""})
+    public behaviorEfficiency?: string // p_behavior_efficiency
+
+    @prop({ default: ""})
+    public workMotivation?: string // p_work_motivation
 
     public getModel() {
         return this.getModelForClass(Preset)
