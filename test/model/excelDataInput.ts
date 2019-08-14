ef46bfef512c3d3a6d5410d2cdc8e24d2a9ae6c3
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-14 13:01:10 +0800

    加入 vscode 调试

--- test/model/excelDataInput.ts
+++ test/model/excelDataInput.ts
@@ -1,8 +1,31 @@
 import { slow, suite, test, timeout } from "mocha-typescript"
 import PhLogger from "../../src/logger/phLogger"
+import mongoose = require("mongoose")
+import XLSX = require("xlsx")
 
 @suite class ExcelDataInput {
+    
+    public before() {
+        PhLogger.info(`before starting the test`)
+        mongoose.connect("mongodb://192.168.100.176:27017/pharbers-ntm-client-2")
+    }
+
     @test public excelModelData() {
         PhLogger.info(`start input data with excel`)
+        const file = "../../../test/data/tm.xlsx"
+        /**
+         * 1. read hospital data in the excel
+         * and collect all the insertion ids
+         */
+        PhLogger.info(`1. read hospital data in the excel`)
+        var wb = XLSX.readFile(file);
+	    /* generate array of arrays */
+	    const data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[1]], {header:1});
+	    console.log(data);
+    }
+
+    public after() {
+        PhLogger.info(`before starting the test`)
+        mongoose.disconnect()
     }
 }
