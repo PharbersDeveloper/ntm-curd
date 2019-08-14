46b09b4a3d08a3d9d560c76853c411fa36108026
Chenghao Sun <601330984@qq.com>
2019-08-14 12:28:05 +0800

    参数化

---
+++ src/configFactory/envConf.ts
@@ -0,0 +1,16 @@
+"use strict"
+import {JsonObject, JsonProperty} from "json2typescript"
+
+@JsonObject("EnvConf")
+export class EnvConf {
+
+    @JsonProperty("oauthHost", String)
+    public oauthHost: string = undefined
+
+    @JsonProperty("oauthPort", Number)
+    public oauthPort: number = undefined
+
+    @JsonProperty("oauthApiNamespace", String)
+    public oauthApiNamespace: string = undefined
+
+}
