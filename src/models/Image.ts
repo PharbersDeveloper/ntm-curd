867400f8f9fac0095428063915e72e228d7541a7
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-16 13:12:24 +0800

    数据导入有图片

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -1,16 +1,22 @@
 "use strict"
+import { JsonObject, JsonProperty } from "json2typescript"
 import { prop, Ref, Typegoose } from "typegoose"
 import IModelBase from "./modelBase"
 
+@JsonObject("Image")
 class Image extends Typegoose implements IModelBase<Image> {
-    @prop({ required: true })
-    public img: string
 
-    @prop({ required: true })
-    public tag: string
+    @JsonProperty("img", String)
+    @prop({ required: true, default: "" })
+    public img: string = ""
 
-    @prop({ required: true })
-    public flag: number
+    @JsonProperty("tag", String)
+    @prop({ required: true, default: "" })
+    public tag: string = ""
+
+    @JsonProperty("flag", Number)
+    @prop({ required: true, default: 0 })
+    public flag: number = 0
 
     public getModel() {
         return this.getModelForClass(Image)
